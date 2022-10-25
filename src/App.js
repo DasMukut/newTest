0eff8cfb495e363fbf5cda0a8ddd762cc90a40b6
Mukut Das <mukutdas97@gmail.com>
2022-10-25 17:48:17 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React,  { useEffect, useState } from "react";
 
 import AddItem from "./components/AddItem";
 import ItemList from "./components/ItemList";
